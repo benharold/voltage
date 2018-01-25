90bfcf7a92f3fa6fd75cfd4cb041ebff56e5484c
Ben Harold <benharold@mac.com>
2018-01-25 15:10:18 -0500

    Initial Commit

---
+++ c-lightning-wallet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  c-lightning-wallet
+//
+//  Created by Ben Harold on 1/25/18.
+//  Copyright © 2018 Harold Consulting. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
