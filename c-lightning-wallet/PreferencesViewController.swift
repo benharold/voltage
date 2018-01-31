a5d0270422701255c5f3842d77cd45e91ecaa412
Ben Harold <benharold@mac.com>
2018-01-30 19:05:03 -0500

    add preferences placeholder

---
+++ c-lightning-wallet/PreferencesViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PreferencesViewController.swift
+//  c-lightning-wallet
+//
+//  Created by Ben Harold on 1/30/18.
+//  Copyright © 2018 Harold Consulting. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+}
