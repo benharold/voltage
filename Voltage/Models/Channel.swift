6b19e2b0d2557450a80367cc2d7f780e3913cf3b
Ben Harold <benharold@mac.com>
2018-02-07 13:37:22 -0500

    remove peer channel definition

--- Voltage/Models/Channel.swift
+++ Voltage/Models/Channel.swift
@@ -8,26 +8,6 @@
 
 import Foundation
 
-//struct Channel: Codable {
-//    let state: String
-//    let owner: String?
-//    let short_channel_id: String?
-//    let funding_txid: String?
-//    let msatoshi_to_us: Int?
-//    let msatoshi_total: Int?
-//    let dust_limit_satoshis: Int
-//    // TODO: This is bigger than 64 bits
-//    //let max_htlc_value_in_flight_msat: BigInt
-//    let channel_reserve_satoshis: Int
-//    let htlc_minimum_msat: Int
-//    let to_self_delay: Int
-//    let max_accepted_htlcs: Int
-//
-//    enum ChannelKeys: String, CodingKey {
-//        case channels
-//    }
-//}
-
 struct Channel: Codable {
     let source: String
     let destination: String
