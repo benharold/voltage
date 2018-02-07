eecbfa762a9678321d4497ed03e40cf7a2dca779
Ben Harold <benharold@mac.com>
2018-02-07 14:01:18 -0500

    add ability to send money

---
+++ Voltage/View Controllers/SendMoneyViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SendMoneyViewController.swift
+//  Voltage
+//
+//  Created by Ben Harold on 2/2/18.
+//  Copyright © 2018 Harold Consulting. All rights reserved.
+//
+
+import Cocoa
+
+class SendMoneyViewController: NSViewController {
+
+    @IBOutlet weak var send_to_field: NSTextField!
+    
+    @IBOutlet weak var amount_field: NSTextField!
+    
+    @IBAction func send_button(_ sender: Any) {
+        send_money()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    func send_money() {
+        let amount = amount_field.stringValue
+        let address = send_to_field.stringValue
+        let decoder: JSONDecoder = JSONDecoder.init()
+        let service: LightningRPCSocket = LightningRPCSocket.create()
+        let newaddr: LightningRPCQuery = LightningRPCQuery(id: Int(getpid()), method: "withdraw", params: [address, amount])
+        let response: Data = service.send(query: newaddr)
+        print(response.to_string())
+        
+        do {
+            let result: Withdraw = try decoder.decode(WithdrawResult.self, from: response).result
+            print(result)
+//            return result
+        } catch {
+            do {
+                let error_message = try decoder.decode(ErrorResult.self, from: response).error
+                print("RPC error: " + error_message)
+            } catch {
+                print("RPC error: \(error)")
+            }
+            print("AddressService JSON decoder error: \(error)")
+        }
+    }
+    
+}
