f9cdc30533c360379381072d647877840f88c64c
Ben Harold <benharold@mac.com>
2018-07-16 18:29:51 -0400

    move preference strings to their own struct

---
+++ Voltage/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Voltage
+//
+//  Created by Ben Harold on 7/16/18.
+//  Copyright © 2018 Harold Consulting. All rights reserved.
+//
+
+import Foundation
+
+struct PreferenceDefaults {
+    static let socket_path_key = "socket_path"
+    static let socket_path = "~/.lightning/lightning-rpc"
+}
