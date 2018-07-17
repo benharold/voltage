e5fe04db810780d961f9e515fd67b4e4c087dafd
Ben Harold <benharold@mac.com>
2018-07-17 14:03:31 -0400

    show and hide specific invoice fields depending on context

--- Voltage/View Controllers/InvoiceViewController.swift
+++ Voltage/View Controllers/InvoiceViewController.swift
@@ -29,25 +29,32 @@ class InvoiceViewController: NSViewController {
     @IBOutlet weak var pay_index: NSTextField!
     @IBOutlet weak var msatoshi_received: NSTextField!
     @IBOutlet weak var paid_at: NSTextField!
-
+    @IBOutlet weak var lower_fields: NSView!
+    @IBOutlet weak var y_constraint: NSLayoutConstraint!
+    
     func display(sender: AnyObject) {
         self.presentViewControllerAsSheet(invoice_view_controller)
     }
     
     func set_created(invoice: CreatedInvoice) {
+        hide_lower_fields()
         self.payment_hash.stringValue = invoice.payment_hash
         expires.stringValue = invoice.expiry_time.to_date_string()
         bolt_11.stringValue = invoice.bolt11
+        if let label: String = invoice.label {
+            self.title = label
+        }
     }
     
     func set_existing(invoice: Invoice) {
-        print(invoice)
+        show_lower_fields()
         self.title = invoice.label
         payment_hash.stringValue = invoice.payment_hash
-        if let bolt11: String = invoice.bolt11 {
+        if let bolt11: String = invoice.bolt12 {
+            show_bolt_11()
             bolt_11.stringValue = bolt11
         } else {
-            bolt_11.stringValue = ""
+            hide_bolt_11()
         }
         msatoshi.intValue = Int32(invoice.msatoshi)
         status.stringValue = invoice.status
@@ -69,5 +76,26 @@ class InvoiceViewController: NSViewController {
         expires.stringValue = invoice.expires_at.to_date_string()
 
         print("done")
+    }
+    
+    func hide_bolt_11() {
+        bolt_11.isHidden = true
+        bolt_11_label.isHidden = true
+        y_constraint.constant = 50
+    }
+    
+    func show_bolt_11() {
+        bolt_11.isHidden = false
+        bolt_11_label.isHidden = false
+        y_constraint.constant = 110
+    }
+    
+    func hide_lower_fields() {
+        show_bolt_11()
+        lower_fields.isHidden = true
+    }
+    
+    func show_lower_fields() {
+        lower_fields.isHidden = false
     }
 }
