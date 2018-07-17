1e5f25641207d887f03da5074bf8ea4bc178ae72
Ben Harold <benharold@mac.com>
2018-07-17 14:00:39 -0400

    allow storage of the invoice label

--- Voltage/Models/JSON-RPC Methods/createdinvoice.swift
+++ Voltage/Models/JSON-RPC Methods/createdinvoice.swift
@@ -8,11 +8,16 @@
 
 import Foundation
 
+// JSON decoder for an object returned by the `invoice` RPC
 struct CreatedInvoice: Codable {
     let payment_hash: String
     let expiry_time: Int
     let expires_at: Int
     let bolt11: String
+    // The invoice label is not returned by the `invoice` RPC nor the
+    // `decodepay` RPC but it is used as the title of the invoice window. That's
+    // why it is allowed to be manually set here.
+    var label: String?
 }
 
 struct CreatedInvoiceResult: Codable {
