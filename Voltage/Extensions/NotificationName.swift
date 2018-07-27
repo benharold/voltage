ecfde52537adedbd788f25393b05406fe298c7cb
Ben Harold <benharold@mac.com>
2018-07-26 22:25:04 -0400

    alphabetize and add a couple

--- Voltage/Extensions/NotificationName.swift
+++ Voltage/Extensions/NotificationName.swift
@@ -9,8 +9,10 @@
 import Foundation
 
 extension Notification.Name {
-    static let loading_start = Notification.Name("loading_start")
+    static let debug_message = Notification.Name("debug_message")
     static let loading_finish = Notification.Name("loading_finish")
-    static let rpc_error = Notification.Name("rpc_error")
+    static let loading_start = Notification.Name("loading_start")
     static let reload = Notification.Name("reload")
+    static let rpc_error = Notification.Name("rpc_error")
+    static let socket_error = Notification.Name("socket_error")
 }
